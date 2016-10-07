8fb9e96f23ff334a450a237adedc19c281b5f91d
Clay Cribbs <lclay.cribbs@gmail.com>
2016-10-07 16:48:17 -0400

    added Dashboard link

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   resources :vehicles
   resources :delivery_trucks
+
+  root 'welcome#index'
+  
+  get 'dashboard' => 'welcome#index'
+  
   get 'main' => 'welcome#about'
 
   get 'delivery_trucks_index' => 'delivery_trucks#index'
 
+	get 'trucks' => 'delivery_trucks#index'
 
-  root 'welcome#index'
-
-  get 'trucks' => 'delivery_trucks#index'
   get 'vehicles' => 'vehicles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
