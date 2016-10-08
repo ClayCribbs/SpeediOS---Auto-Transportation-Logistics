505587ae6f4d9e34413f99aaf97757d1c348a476
Clay Cribbs <lclay.cribbs@gmail.com>
2016-10-08 13:20:53 -0400

    removed conflicting code

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,30 +1,4 @@
 class Vehicle < ApplicationRecord
-	before_save :geocode_endpoints
-
-
-  def address
-    origin.to_s
-    destination.to_s
-  end
-
-  def geocode_endpoints
-
-    if origin_changed?
-      geocoded = Geocoder.search(origin).first
-      if geocoded
-        self.origin_latitude = geocoded.latitude
-        self.origin_longitude = geocoded.longitude
-      end
-    end
-
-    if destination_changed?
-    	geocoded = Geocoder.search(destination).first
-      if geocoded
-        self.destination_latitude = geocoded.destination_latitude
-        self.destination_latitude = geocoded.destination_longitude
-      end
-    end
-
-  end
+	
 
 end
