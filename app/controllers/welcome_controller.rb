5413b1c74880870667a4e714bee48ae256fe740b
Clay Cribbs <lclay.cribbs@gmail.com>
2016-10-09 18:16:29 -0400

    Added Drop down to select destination on Dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,23 @@
 class WelcomeController < ApplicationController
-
   def index
+
   	@delivery_trucks = DeliveryTruck.all
   	@vehicles = Vehicle.all
 
+    @destinations = Vehicle.select(:destination).order(:destination).distinct
+
   	@vehicles.each do |vehicle|
       if vehicle.distance == nil
-    	directions = GoogleDirections.new(vehicle.origin, vehicle.destination) 
-      vehicle.update(distance: directions.distance_in_miles)
+    	   directions = GoogleDirections.new(vehicle.origin, vehicle.destination) 
+         vehicle.update(distance: directions.distance_in_miles)
       end
   	end
-  
-  end
 
-  def about
+    if params[:destination]== nil
+      @tableVehicles = Vehicle.all
+    else
+      @tableVehicles = Vehicle.where("destination LIKE ?", params[:destination])
+    end
+
   end
 end
