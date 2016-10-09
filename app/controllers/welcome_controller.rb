acf05d63c89f175ec4e1b1fe688301d09ef69463
Clay Cribbs <lclay.cribbs@gmail.com>
2016-10-09 12:52:33 -0400

    Make sure you rails db:reset if you get any errors after updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,14 @@
 class WelcomeController < ApplicationController
+
   def index
   	@delivery_trucks = DeliveryTruck.all
   	@vehicles = Vehicle.all
+
   	@vehicles.each do |vehicle|
     	directions = GoogleDirections.new(vehicle.origin, vehicle.destination) 
-    	distance_in_miles = directions.distance_in_miles
-    	vehicle.distance = distance_in_miles 
+      vehicle.update(distance: directions.distance_in_miles)
   	end
+  
   end
 
   def about
