f0d9170b596a086d8c77f61e356844ffb61dfce3
Clay Cribbs <lclay.cribbs@gmail.com>
2016-10-10 10:56:44 -0400

    Requires db: reset - changed dropdowns to reflect current status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009223315) do
+ActiveRecord::Schema.define(version: 20161010133904) do
 
   create_table "delivery_trucks", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161009223315) do
     t.string   "requestedArrivalTime"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.float    "currentWeight"
   end
 
   create_table "users", force: :cascade do |t|
