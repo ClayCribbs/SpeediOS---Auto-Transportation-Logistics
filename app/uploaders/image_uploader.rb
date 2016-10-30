67023c0ca0a4c714bc362d7283c61754c4745461
Clay Cribbs <lclay.cribbs@gmail.com>
2016-10-30 17:15:40 -0400

    heroku fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,12 +7,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  group :development do
-  storage :file
-end
-group :production do 
-  storage :fog
-end
+if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
