df2ec72fd1b0af57528720f7f012a1c4962c04f6
Clay Cribbs <lclay.cribbs@gmail.com>
2016-11-09 16:25:23 -0500

    Changing timing, trying to get dashboard to refresh correctly

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,8 +4,7 @@ function loadTables() {
 	setTimeout(function() {
 		$('#contracts').load("dashboard #contracts")
 		$('#widget').load("dashboard #widget")
-		$('#order-table').load()
-	},  500);  
+	},  1000);  
 }
 
 
