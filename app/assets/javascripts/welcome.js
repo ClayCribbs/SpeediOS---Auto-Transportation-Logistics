c5140f8ac0ef6739bf01026d72f1752346553ccf
Clay Cribbs <lclay.cribbs@gmail.com>
2016-11-09 17:05:54 -0500

    adjust timing

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,20 +4,16 @@ function loadTables() {
 	setTimeout(function() {
 		$('#contracts').load("dashboard #contracts")
 		$('#widget').load("dashboard #widget")
-		$('#order-table').load()
 	},  200);  
 
 	setTimeout(function() {
-		$('#order-table').load()
 		$('#widget').load("dashboard #widget")
 		$('#contracts').load("dashboard #contracts")
 	},  5000);  
 
 		setTimeout(function() {
-		$('#contracts').load("dashboard #contracts")
 		$('#widget').load("dashboard #widget")
-		$('#order-table').load()
-	},  8000);  
+	},  5000);  
 }
 
 
