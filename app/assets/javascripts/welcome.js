75514f73dc94da60db327c2295e2125c0e0598b3
Clay Cribbs <lclay.cribbs@gmail.com>
2016-11-09 17:00:02 -0500

    tweak dashboard timing

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -8,10 +8,16 @@ function loadTables() {
 	},  200);  
 
 	setTimeout(function() {
+		$('#order-table').load()
+		$('#widget').load("dashboard #widget")
+		$('#contracts').load("dashboard #contracts")
+	},  5000);  
+
+		setTimeout(function() {
 		$('#contracts').load("dashboard #contracts")
 		$('#widget').load("dashboard #widget")
 		$('#order-table').load()
-	},  5000);  
+	},  8000);  
 }
 
 
