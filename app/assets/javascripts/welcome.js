48ed487fa052a4292035e69149080ac113e67ee0
Clay Cribbs <lclay.cribbs@gmail.com>
2016-11-09 19:19:47 -0500

    update dashboard widgets

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -7,13 +7,23 @@ function loadTables() {
 	},  200);  
 
 	setTimeout(function() {
-		$('#widget').load("dashboard #widget")
 		$('#contracts').load("dashboard #contracts")
 	},  5000);  
 
 		setTimeout(function() {
 		$('#widget').load("dashboard #widget")
+	},  5000); 
+
+	setTimeout(function() {
+		$('#widget2').load("dashboard #widget")
+	},  5000);   
+	setTimeout(function() {
+		$('#widget3').load("dashboard #widget")
 	},  5000);  
+	setTimeout(function() {
+		$('#widget4').load("dashboard #widget")
+	},  5000);  
+
 }
 
 
